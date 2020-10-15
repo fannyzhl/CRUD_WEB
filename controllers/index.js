626caf08b762f5e7e7a4e20f444a49d3306c5234
fanny <fannyzhl583@gmail.com>
2020-10-14 21:50:35 -0400

    borrar imagenes y cambiar los nombres de las carpetas

--- controllers/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-
-let router = express.Router()
-
-router.use('/user', require('./user'));
-router.use('/product', require('./product')); 
-router.use('/car', require('./car'));
-
-module.exports = router;
--- helpers/db.js
+++
@@ -1,5 +0,0 @@
-const config = require('./config');
-const pgp = require('pg-promise')();
-const db = pgp(config.url);
-
-module.exports = db;
